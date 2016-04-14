e060a8525bf36bd1a241609f4850d6651b33c5d3
PIYUSH GUPTA <opacuspiyush@gmail.com>
2016-04-14 12:48:47 -0700

    Update XMLParser.py

--- XMLParser.py
+++ XMLParser.py
@@ -1,61 +1,130 @@
-import urllib, urllib2, time
 import StringIO
+import pattern.fr
+import pattern.de
 from xml.dom.minidom import parse
 import sys
 import os
 import json
+from textblob_de import TextBlobDE as TextBlob
 
 
-inputFilePath = sys.argv[1]
-inputFile = open(inputFilePath)
-the_page = inputFile.read()
-inputFile.close()
+def getFeatureDict(review_Dict, lang):
+    feature_dict = {}
+    print(lang)
+    for keys in review_Dict:
+        features_list = [] # list of identified feature,descriptor pair
+        for review in review_Dict[keys]: # for each review for a title
+            # print(pattern.fr.tag(review))
+            review_list = review.split(".")
+            # for each line in a review
+            for each_review in review_list:
+                if lang == 'fr':
+                    tagged_review = pattern.fr.tag(each_review, tokenize=True)
+                elif lang == 'de':
+                    tagged_review = pattern.de.tag(each_review, tokenize=True)
+                else:
+                    tagged_review = " "
+                print(tagged_review)
+                features_present = False
+                for i in range(0, len(tagged_review)):
+                    # check for the possible tag combinations
 
-directory = os.path.dirname(inputFilePath)
-print directory
-inputFileName = os.path.basename(inputFilePath)
+                    if tagged_review[i][1] == "JJ": # check for tag JJ
+                        features_present = True
+                        feature = None
+                        descriptor = tagged_review[i][0]
+                        if i+1 < len(tagged_review):
+                            if tagged_review[i+1][1] == "NN" or tagged_review[i+1][1] == "NNS":
+                                feature = tagged_review[i+1][0]
+                        if i-1 >= 0 and feature is None:
+                            if tagged_review[i-1][1] == "NN" or tagged_review[i-1][1] == "NNS":
+                                feature = tagged_review[i-1][0]
+                        if feature is None:
+                            feature_back = None
+                            feature_front = None
+                            j = i+1
+                            k = i-1
+                            while j < len(tagged_review):
+                                if tagged_review[j][1] == "NN" or tagged_review[j][1] == "NNS":
+                                    feature_back = tagged_review[j][0]
+                                    break
+                                j += 1
+                            while k >= 0:
+                                if tagged_review[k][1] == "NN" or tagged_review[k][1] == "NNS":
+                                    feature_front = tagged_review[k][0]
+                                    break
+                                k -= 1
+                            if (j-i) < (i-k) and feature_back is not None:
+                                feature = feature_back
+                            elif feature_front is not None:
+                                feature = feature_front
+                        features_list.append((descriptor, feature))
+
+                    if tagged_review[i][1] == "RB":
+                        if i+1 < len(tagged_review):
+                            if tagged_review[i+1][1] == "VBN":
+                                features_present = True
+                                descriptor = tagged_review[i][0]
+                                feature = tagged_review[i+1][0]
+                                features_list.append((descriptor, feature))
+
+                    if tagged_review[i][1] == "RB":
+                        if i+1 < len(tagged_review):
+                            if tagged_review[i+1][1] == "NN" or tagged_review[i+1][1] == "NNS":
+                                features_present = True
+                                descriptor = tagged_review[i][0]
+                                feature = tagged_review[i+1][0]
+                                features_list.append((descriptor, feature))
 
 
-ouputFile = open(directory + "\\" + inputFileName + "_parsed.txt" ,'w')
-#inputFile = open(r"C:\Users\Piyush\Desktop\NLP Project\Corpus\trainreview",'rU')
-#the_page = inputFile.read() 
-#inputFile.close()
-#ouputFile = open("trainreview_parsed_books.txt",'w')
+                #append sentiment for the line which has features present
+                if features_present:
+                    if lang == 'fr':
+                        features_list.append(pattern.fr.sentiment(each_review))
+                    elif lang == 'de':
+                        features_list.append(TextBlob(each_review).sentiment)
 
-dict = {}
-def WriteTagInFile(tag,tagName):
+        # display all the feature pairs along with the sentiments
+        for each_pair in features_list:
+            if type(each_pair[0]) == unicode:
+                if each_pair[0] is not None and each_pair[1] is not None:
+                    print(each_pair[0].encode('utf-8') +" "+ each_pair[1].encode('utf-8'))
+            else:
+                print(each_pair)
+        print
+        # print(pattern.fr.sentiment(review_Dict[keys][0]))
+        # print(pattern.fr.tag(review_Dict[keys][0], tokenize=True))
+        # print
+        # s = pattern.fr.parsetree(review_Dict[keys][0],relations=True, lemmata=True)
+        # for sentence in s:
+        #     for chunk in sentence.chunks:
+        #         for word in chunk.words:
+        #             print word,
+        #         print
+        #     print
 
-     if(tag.hasChildNodes is not None and len(tag.childNodes) > 0 and  tag.childNodes[0].data is not None):
-        ouputFile.write(tagName)
-        ouputFile.write("\n")
-        ouputFile.write((tag.childNodes[0].data).encode("utf-8"))
-        ouputFile.write("\n")
 
-def formDictiorany(title,review):
+def parseInput():
+    inputFile = open(sys.argv[1])
+    books_reviews_list, dvd_reviews_list, music_reviews_list = json.load(inputFile)
 
-     if(dict.has_key(title)):
-           dict[title].append(review);
-     else:
-           dict[title] = [review]
+    for each in books_reviews_list:
+        getFeatureDict(each[1], each[0])
 
-#print the_page 
-dom = parse(StringIO.StringIO(the_page))
-name = dom.getElementsByTagName('item')
-#name[0].firstChild
-for child in name:
-    title = child.getElementsByTagName('title')[0]
-    text = child.getElementsByTagName('text')[0]
+    # for each in dvd_reviews_list:
+    #     getFeatureDict(each[1], each[0])
+    #
+    # for each in music_reviews_list:
+    #     getFeatureDict(each[1], each[0])
 
-    if(title.hasChildNodes is not None and len(title.childNodes) > 0 and  title.childNodes[0].data is not None
-        and text.hasChildNodes is not None and len(text.childNodes) > 0 and  text.childNodes[0].data is not None):
-        key  = title.childNodes[0].data
-        value = text.childNodes[0].data
-        
-        formDictiorany(key,value)
+    inputFile.close()
 
-for key in dict:
-    ouputFile.write(key.encode("utf-8"))
-    ouputFile.write("\n")
-    ouputFile.write(str(dict[key]))
-    ouputFile.write("\n")
-    ouputFile.write("\n")
+
+
+if __name__ == '__main__':
+    parseInput()
+
+
+
+
+
