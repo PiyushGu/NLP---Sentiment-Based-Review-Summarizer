cc63415cded7ea1b1c1673863b5fb2e6399b4214
PIYUSH GUPTA <opacuspiyush@gmail.com>
2016-04-17 18:40:27 -0700

    Tagging Shallow Parsing

--- Tagging_Shallow_Parsing.py
+++ Tagging_Shallow_Parsing.py
@@ -1,19 +1,20 @@
-import StringIO
 import pattern.fr
 import pattern.de
-from xml.dom.minidom import parse
 import sys
-import os
 import json
-from textblob_de import TextBlobDE as TextBlob
+import codecs
+
+
+def dependency_parser(review):
+    s = pattern.fr.parse(review).split()
 
 
 def getFeatureDict(review_Dict, lang):
-    feature_dict = {}
-    print(lang)
+    summarized_dict = {}
     for keys in review_Dict:
-        features_list = [] # list of identified feature,descriptor pair
+        feature_list = []
         for review in review_Dict[keys]: # for each review for a title
+            dependency_parser(review)
             # print(pattern.fr.tag(review))
             review_list = review.split(".")
             # for each line in a review
@@ -24,9 +25,8 @@ def getFeatureDict(review_Dict, lang):
                     tagged_review = pattern.de.tag(each_review, tokenize=True)
                 else:
                     tagged_review = " "
-                print(tagged_review)
-                features_present = False
                 for i in range(0, len(tagged_review)):
+                    features_present = False
                     # check for the possible tag combinations
 
                     if tagged_review[i][1] == "JJ": # check for tag JJ
@@ -54,19 +54,14 @@ def getFeatureDict(review_Dict, lang):
                                     feature_front = tagged_review[k][0]
                                     break
                                 k -= 1
-                            if (j-i) < (i-k) and feature_back is not None:
+                            if feature_front is None and feature_back is not None:
                                 feature = feature_back
-                            elif feature_front is not None:
+                            elif feature_back is None and feature_front is not None:
+                                feature_back = feature_front
+                            elif (j-i) < (i-k):
+                                feature = feature_back
+                            else:
                                 feature = feature_front
-                        features_list.append((descriptor, feature))
-
-                    if tagged_review[i][1] == "RB":
-                        if i+1 < len(tagged_review):
-                            if tagged_review[i+1][1] == "VBN":
-                                features_present = True
-                                descriptor = tagged_review[i][0]
-                                feature = tagged_review[i+1][0]
-                                features_list.append((descriptor, feature))
 
                     if tagged_review[i][1] == "RB":
                         if i+1 < len(tagged_review):
@@ -74,51 +69,48 @@ def getFeatureDict(review_Dict, lang):
                                 features_present = True
                                 descriptor = tagged_review[i][0]
                                 feature = tagged_review[i+1][0]
-                                features_list.append((descriptor, feature))
 
+                    if tagged_review[i][1] == "NN":
+                        if i+1 < len(tagged_review):
+                            if tagged_review[i+1][1] == "NN" or tagged_review[i+1][1] == "NNS":
+                                features_present = True
+                                descriptor = tagged_review[i][0]
+                                feature = tagged_review[i+1][0]
 
-                #append sentiment for the line which has features present
-                if features_present:
-                    if lang == 'fr':
-                        features_list.append(pattern.fr.sentiment(each_review))
-                    elif lang == 'de':
-                        features_list.append(TextBlob(each_review).sentiment)
+                    # append the descriptor feature pair onl if the sentiment is not 0
+                    if features_present:
+                        if descriptor != None and feature != None:
+                            feature_list.append((descriptor, feature))
+        summarized_dict[keys] = feature_list
+    return summarized_dict
 
-        # display all the feature pairs along with the sentiments
-        for each_pair in features_list:
-            if type(each_pair[0]) == unicode:
-                if each_pair[0] is not None and each_pair[1] is not None:
-                    print(each_pair[0].encode('utf-8') +" "+ each_pair[1].encode('utf-8'))
-            else:
-                print(each_pair)
-        print
-        # print(pattern.fr.sentiment(review_Dict[keys][0]))
-        # print(pattern.fr.tag(review_Dict[keys][0], tokenize=True))
-        # print
-        # s = pattern.fr.parsetree(review_Dict[keys][0],relations=True, lemmata=True)
-        # for sentence in s:
-        #     for chunk in sentence.chunks:
-        #         for word in chunk.words:
-        #             print word,
-        #         print
-        #     print
 
 
 def parseInput():
     inputFile = open(sys.argv[1])
     books_reviews_list, dvd_reviews_list, music_reviews_list = json.load(inputFile)
+    books_summary = []
+    dvd_summary = []
+    music_summary = []
 
     for each in books_reviews_list:
-        getFeatureDict(each[1], each[0])
+        summary = getFeatureDict(each[1], each[0])
+        books_summary.append((each[0], summary))
 
-    # for each in dvd_reviews_list:
-    #     getFeatureDict(each[1], each[0])
-    #
-    # for each in music_reviews_list:
-    #     getFeatureDict(each[1], each[0])
+    for each in dvd_reviews_list:
+        summary = getFeatureDict(each[1], each[0])
+        dvd_summary.append((each[0], summary))
 
+    for each in music_reviews_list:
+        summary = getFeatureDict(each[1], each[0])
+        music_summary.append((each[0], summary))
     inputFile.close()
 
+    summaries = [books_summary, dvd_summary, music_summary]
+    jsonData = json.dumps(summaries)
+    outFile = open("summarized_list",'w')
+    outFile.write(jsonData)
+    outFile.close()
 
 
 if __name__ == '__main__':
