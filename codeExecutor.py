93c6aa11ec88d175c9783c4a1a11d22135d2f30c
PIYUSH GUPTA <opacuspiyush@gmail.com>
2016-04-29 22:02:23 -0700

    Code Executor(to execute the program automatically)
    executing each script in order

--- codeExecutor.py
+++ codeExecutor.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-
+#executing each script in order
 os.system('python Review_Parser.py ' + sys.argv[1])
 os.system('python Tagging_Shallow_Parsing.py XML_Parsed_Dictionary.txt')
 os.system('python Sentiment_Syntactic_Analyser.py Summarized_Review_List.txt')
