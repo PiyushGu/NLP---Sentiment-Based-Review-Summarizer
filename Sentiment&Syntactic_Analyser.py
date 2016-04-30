2bee49f6fae2d8a7a079b5613804c2f09ca7abfd
PIYUSH GUPTA <opacuspiyush@gmail.com>
2016-04-29 21:23:26 -0700

    Update Sentiment&Syntactic_Analyser.py

--- Sentiment&Syntactic_Analyser.py
+++ Sentiment&Syntactic_Analyser.py
@@ -5,7 +5,6 @@ import subprocess
 import os
 from multiprocessing import pool
 
-
 def sentiment_analyser(summary, lang):
     final_features = {}
     for title in summary:
@@ -87,77 +86,88 @@ if __name__ == '__main__':
     books_final_features = []
     dvd_final_features = []
     music_final_features = []
-    outFile = open("final_feature_list.txt", 'w')
+    os.makedirs("Summary")
 
     for each in books_summary:
-        print("Books Summary for "+each[0]+" language")
+        if each[0] == "fr":
+            books_outFile = open("Summary/Books_Summarized_Reviews_fr.txt", 'w')
+        elif each[0] == "de":
+            books_outFile = open("Summary/Books_Summarized_Reviews_de.txt", 'w')
         summary = sentiment_analyser(each[1], each[0])
 
         for title in summary:
-            outFile.write(title.encode("utf-8"))
+            books_outFile.write(title.encode("utf-8"))
+            books_outFile.write("\n-------------------------------------------------------------------\n")
+
 
             if len(summary[title][0]) > 0:
-                outFile.write("\n\nPositive Reviews\n")
+                books_outFile.write("\n\nPositive Reviews\n")
+                books_outFile.write("--------------------\n")
             for feature in summary[title][0]:
-                outFile.write(feature[0].encode("utf-8")+"-"+ feature[1].encode("utf-8"))
-                outFile.write("\n")
+                books_outFile.write(feature[0].encode("utf-8") + " " + feature[1].encode("utf-8"))
+                books_outFile.write("\n")
 
             if len(summary[title][1]) > 0:
-                outFile.write("\nNegative Reviews\n")
+                books_outFile.write("\nNegative Reviews\n")
+                books_outFile.write("--------------------\n")
             for feature in summary[title][1]:
-                outFile.write(feature[0].encode("utf-8")+"-"+ feature[1].encode("utf-8"))
-                outFile.write("\n")
+                books_outFile.write(feature[0].encode("utf-8") + " " + feature[1].encode("utf-8"))
+                books_outFile.write("\n")
 
-            outFile.write("\n")
+            books_outFile.write("\n")
 
 
     for each in dvd_summary:
-        print("DVD Summary for "+each[0]+" language")
+        if each[0] == "fr":
+            dvd_outFile = open("Summary/DVDs_Summarized_Reviews_fr.txt", 'w')
+        elif each[0] == "de":
+            dvd_outFile = open("Summary/DVDs_Summarized_Reviews_de.txt", 'w')
         summary = sentiment_analyser(each[1], each[0])
 
         for title in summary:
-            outFile.write(title.encode("utf-8"))
+            dvd_outFile.write(title.encode("utf-8"))
+            dvd_outFile.write("\n-------------------------------------------------------------------\n")
 
             if len(summary[title][0]) > 0:
-                outFile.write("\n\nPositive Reviews\n")
+                dvd_outFile.write("\n\nPositive Reviews\n")
+                dvd_outFile.write("--------------------\n")
             for feature in summary[title][0]:
-                outFile.write(feature[0].encode("utf-8")+"-"+ feature[1].encode("utf-8"))
-                outFile.write("\n")
+                dvd_outFile.write(feature[0].encode("utf-8")+"-"+ feature[1].encode("utf-8"))
+                dvd_outFile.write("\n")
 
             if len(summary[title][1]) > 0:
-                outFile.write("\nNegative Reviews\n")
+                dvd_outFile.write("\nNegative Reviews\n")
+                dvd_outFile.write("--------------------\n")
             for feature in summary[title][1]:
-                outFile.write(feature[0].encode("utf-8")+"-"+ feature[1].encode("utf-8"))
-                outFile.write("\n")
+                dvd_outFile.write(feature[0].encode("utf-8")+"-"+ feature[1].encode("utf-8"))
+                dvd_outFile.write("\n")
 
-            outFile.write("\n")
+            dvd_outFile.write("\n")
 
 
     for each in music_summary:
-        print("Music Summary for "+each[0]+" language")
+        if each[0] == "fr":
+            music_outFile = open("Summary/Music_Summarized_Reviews_fr.txt", 'w')
+        elif each[0] == "de":
+            music_outFile = open("Summary/Music_Summarized_Reviews_de.txt", 'w')
         summary = sentiment_analyser(each[1], each[0])
 
         for title in summary:
-            outFile.write(title.encode("utf-8"))
+            music_outFile.write(title.encode("utf-8"))
+            music_outFile.write("\n-------------------------------------------------------------------\n")
 
             if len(summary[title][0]) > 0:
-                outFile.write("\n\nPositive Reviews\n")
+                music_outFile.write("\n\nPositive Reviews\n")
+                music_outFile.write("--------------------\n")
             for feature in summary[title][0]:
-                outFile.write(feature[0].encode("utf-8")+" "+ feature[1].encode("utf-8"))
-                outFile.write("\n")
+                music_outFile.write(feature[0].encode("utf-8")+" "+ feature[1].encode("utf-8"))
+                music_outFile.write("\n")
 
             if len(summary[title][1]) > 0:
-                outFile.write("\nNegative Reviews\n")
+                music_outFile.write("\nNegative Reviews\n")
+                music_outFile.write("--------------------\n")
             for feature in summary[title][1]:
-                outFile.write(feature[0].encode("utf-8")+" "+ feature[1].encode("utf-8"))
-                outFile.write("\n")
+                music_outFile.write(feature[0].encode("utf-8")+" "+ feature[1].encode("utf-8"))
+                music_outFile.write("\n")
 
-            outFile.write("\n")
-
-    '''final_feature_list = [books_final_features, dvd_final_features, music_final_features]
-    jsonData = json.dumps(final_feature_list)
-
-     outFile = open("final_feature_list.txt", 'w')
-    outFile.write(jsonData)
-    outFile.close()
-    '''
+            music_outFile.write("\n")
