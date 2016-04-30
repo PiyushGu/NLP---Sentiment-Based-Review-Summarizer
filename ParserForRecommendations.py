b0010badad2750c6f21c6c0ae2731234b1358113
PIYUSH GUPTA <opacuspiyush@gmail.com>
2016-04-29 20:56:28 -0700

    XML Parser ( Used to parsed Reviews)
    Updated XML parser used to parse the reviews

--- ParserForRecommendations.py
+++ ParserForRecommendations.py
@@ -5,25 +5,32 @@ import sys
 import os
 import json
 
-# to take input file path 
+# takes input file path 
 inputFilePath = sys.argv[1]
 
-# inputFilePath = r"C:\Users\Piyush\Desktop\NLP Project\Corpus\cls-acl10-unprocessed\fr\dvd\testFile.review"
+# opens the input file
 inputFile = open(inputFilePath, 'rU')
+# reads input file
 the_page = inputFile.read()
+#close input file
 inputFile.close()
 
+# extracting directory name of the input file
 directory = os.path.dirname(inputFilePath)
-print directory
+
+#extracting the file Name fron the path
 inputFileName = os.path.basename(inputFilePath)
 
 #opening output File
 ouputFile = open(directory + "\\" + inputFileName + "parsed_for_recommend.txt", 'w')
 
+#creating the output dictionary
 dict = {}
 
 
-def formDictiorany(key, value):
+#method to form Dictionary 
+def formDictionary(key, value):
+    # check if the key is present in the dictionary or not 
     if (dict.has_key(key)):
         dict[key].append(value);
     else:
@@ -31,21 +38,32 @@ def formDictiorany(key, value):
 
 #print the_page
 dom = parse(StringIO.StringIO(the_page))
+# extracting the all item tags from the given XML
 name = dom.getElementsByTagName('item')
-#name[0].firstChild
+
+#iterating over each item
 for child in name:
+    #get the title attribute from the item
     title = child.getElementsByTagName('title')[0]
+    
+    #get the reviewer attribute from the item
     user = child.getElementsByTagName('reviewer')[0]
+    
+    #get the rating attribute from the item
     rating = child.getElementsByTagName('rating')[0]
 
+    #condition to check if the corresponding tag is present in the item
     if (title.hasChildNodes is not None and len(title.childNodes) > 0 and title.childNodes[0].data is not None
         and user.hasChildNodes is not None and len(user.childNodes) > 0 and user.childNodes[0].data is not None
         and rating.hasChildNodes is not None and len(rating.childNodes) > 0 and rating.childNodes[0].data is not None):
+       
         key = user.childNodes[0].data
         value = (title.childNodes[0].data, rating.childNodes[0].data)
+       
+        #adding the tag in the dictionary
+        formDictionary(key, value)
 
-        formDictiorany(key, value)
-
+#writing output in the dictionary
 for key in sorted(dict):
     for (title, rating) in dict[key]:
         ouputFile.write(key.encode("utf-8") + "\t" +  rating.encode("utf-8")   + "\t" +title.encode("utf-8"))
